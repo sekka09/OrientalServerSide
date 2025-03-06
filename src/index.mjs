ba95c551666da53473d6bd3217bea7be9845a428
sekka09 <161488323+sekka09@users.noreply.github.com>
2025-03-06 22:12:57 +0100

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -15,7 +15,7 @@ const app = express();
             app.use(express.json())
             app.use(cookieParser()) 
             app.use(cors({
-                origin: 'http://http://localhost:3000', // Replace with your frontend's domain
+                origin: 'www.orientalparfum.shop', // Replace with your frontend's domain
                 credentials: true, // Allow cookies
               }));
             app.use(usersrouter)
