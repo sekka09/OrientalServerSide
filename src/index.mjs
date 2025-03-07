34b490411da9e8e316a22b1590253f93565b8b57
sekka09 <161488323+sekka09@users.noreply.github.com>
2025-03-07 07:44:48 +0100

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -16,7 +16,7 @@ const app = express();
             app.use(cookieParser()) 
             app.use(cors({
                 origin: ['https://www.orientalparfum.shop','http://localhost:3000','https://www.orientalparfum.shop/U2FsdGVkX18gWp8/iSKiyhKPB5FWr0dLigHLVCCzwow='],// Replace with your frontend's domain
-                 methods: ["GET", "POST", "PUT", "DELETE"],
+                 methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
                 credentials: true, // Allow cookies
               }));
             app.use(usersrouter)
